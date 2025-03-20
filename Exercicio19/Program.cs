104adab65de0d9e075c563238029bf269eba7647
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 17:00:39 -0300

    Implementado exercício 19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio19;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int somaTotal = 0;
+
+        for (int contador = 1; contador <= 500; contador++)
+        {
+            if (contador % 2 != 0 && contador % 3 == 0)
+                somaTotal += contador;
+        }
+
+        Console.WriteLine($"A soma total dos números ímpares e múltiplos de 3 de 1 à 500 é de {somaTotal}");
+
+        Console.ReadLine();
+    }
+}
