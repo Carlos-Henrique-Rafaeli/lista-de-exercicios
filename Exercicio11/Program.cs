b686f47bc40c80d5d18fa5e01b383cff3189efcb
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 15:46:10 -0300

    Implementado exercício 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Qual é seu nome: ");
+        string nome = Console.ReadLine()!;
+
+        Console.Write("Quantos anos você tem: ");
+        int idadeAnos = Convert.ToInt32(Console.ReadLine());
+
+        int idadeDias = idadeAnos * 365;
+
+        Console.WriteLine($"{nome}, você ja viveu {idadeDias} dias!");
+
+        Console.ReadLine();
+    }
+}
