2e503b24a0361af70a46a464aa7f7cad71d1b8e2
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 17:35:18 -0300

    Implementado exercício 23

---
+++ Exercicio23/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio23;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int contador = 1; contador <= 100; contador++)
+        {
+            if (contador % 3 == 0 && contador % 5 == 0)
+                Console.WriteLine($"FizzBuzz ({contador})");
+            else if (contador % 3 == 0)
+                Console.WriteLine($"Fizz ({contador})");
+            else if (contador % 5 == 0)
+                Console.WriteLine($"Buzz ({contador})");
+            else
+                Console.WriteLine(contador);
+        }
+
+
+        Console.ReadLine();
+    }
+}
