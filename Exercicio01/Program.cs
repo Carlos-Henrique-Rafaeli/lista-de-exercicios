a8754e0b253d0b3e40be1ba4751582984462150b
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 13:56:38 -0300

    Implementado exercício 01

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o comprimento da caixa: ");
+        decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite o largura da caixa: ");
+        decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Digite o altura da caixa: ");
+        decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+        decimal resultado = comprimento * largura * altura;
+
+        Console.WriteLine($"O volume da caixa é de {resultado} cm³");
+
+        Console.ReadLine();
+    }
+}
