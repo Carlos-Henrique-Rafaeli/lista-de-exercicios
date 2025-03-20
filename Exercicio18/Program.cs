77f63ee14743f2617f4a84aa50f53aab9373edf0
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 16:54:37 -0300

    Implementado exercício 18

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio18;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int contador = 100; contador <= 200; contador++)
+        {
+            if (contador % 2 != 0)
+                Console.WriteLine($"O número {contador} é impar");
+        }
+
+        Console.ReadLine();
+    }
+}
