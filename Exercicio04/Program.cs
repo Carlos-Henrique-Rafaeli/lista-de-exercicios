33a1b19f6344f8d3b2c0852df91488cc966ea83d
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 14:39:50 -0300

    Implementado exercício 04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio04;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o valor da temperatura em Celsius (°C): ");
+        double temperaturaCelsius = Convert.ToDouble(Console.ReadLine());
+
+        double temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
+
+        Console.WriteLine($"A temperatura {temperaturaCelsius:F2}(°C) em Fahrenheit é {temperaturaFahrenheit:F2}(°F)");
+
+        Console.ReadLine();
+    }
+}
