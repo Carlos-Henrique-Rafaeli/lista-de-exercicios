ff4e78ce1433c78416674bc267bfc2767e4d739f
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 15:35:42 -0300

    Implementado exercício 09

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio09;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o comprimento do terreno: ");
+        double comprimento = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe a largura do terreno: ");
+        double largura = Convert.ToDouble(Console.ReadLine());
+
+        double area = comprimento * largura;
+
+        Console.WriteLine($"A área do terreno é de {area} m²");
+
+        Console.ReadLine();
+    }
+}
