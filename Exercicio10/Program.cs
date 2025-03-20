e46f0ba983a8b666b2dd5e43d1adb33fcefde4d1
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 15:41:33 -0300

    Implementado exercício 10

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio10;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe a quantidade de pães vendidos: ");
+        double paoFrances = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe a quantidade de broas vendidas: ");
+        double broa = Convert.ToDouble(Console.ReadLine());
+
+        double valorVendaDiaria = (paoFrances * 0.12) + (broa * 1.5);
+
+        double valorParaGuardar = valorVendaDiaria * 0.1;
+
+        Console.WriteLine($"No final do dia foi arrecadado {valorVendaDiaria:F2} reais");
+        Console.WriteLine($"Desses é recomendado guardar na poupança {valorParaGuardar:F2} reais");
+
+        Console.ReadLine();
+    }
+}
