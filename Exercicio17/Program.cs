b43449cd5c393265f552f3024a214fc097dd8ebd
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 16:50:51 -0300

    Implementado exercício 17

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio17;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o valor de A: ");
+        int valorA = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Informe o valor de B: ");
+        int valorB = Convert.ToInt32(Console.ReadLine());
+
+        int resultado;
+
+        if (valorA == valorB)
+        {
+            resultado = valorA + valorB;
+            Console.WriteLine($"Os valores de A({valorA}) e B({valorB}) são iguais");
+            Console.WriteLine($"A soma deles ficou {resultado}");
+        }
+        else
+        {
+            resultado = valorA * valorB;
+            Console.WriteLine($"Os valores de A({valorA}) e B({valorB}) são diferentes");
+            Console.WriteLine($"A multiplicação deles ficou {resultado}");
+        }
+
+
+        Console.ReadLine();
+    }
+}
