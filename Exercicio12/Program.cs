87a7f74ccb7aa27dbf554c254c6eb815dd212e38
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 15:52:37 -0300

    Implementado exercício 12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio12;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe seu salário atual: ");
+        double salarioAtual = Convert.ToDouble(Console.ReadLine());
+
+        double salarioAumento = salarioAtual + (salarioAtual * 0.15);
+
+        double salarioDesconto = salarioAumento - (salarioAumento * 0.08);
+
+        Console.WriteLine($"Seu sálario de R${salarioAtual:F2} com o aumento era para ficar R${salarioAumento:F2},\nMas com impostos ficou R${salarioDesconto:F2}!");
+
+        Console.ReadLine();
+    }
+}
