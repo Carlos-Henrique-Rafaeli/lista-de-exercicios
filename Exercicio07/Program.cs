ee4dee77d45236714be11d52070f0a34151e7ed6
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 15:18:21 -0300

    Implementado exercício 07

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio07;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe a nota da primeira prova: ");
+        double primeiraNota = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o peso da primeira prova: ");
+        double primeiroPeso = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe a nota da segunda prova: ");
+        double segundaNota = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o peso da segunda prova: ");
+        double segundoPeso = Convert.ToDouble(Console.ReadLine());
+
+        double somaNotas = (primeiraNota * primeiroPeso) + (segundaNota * segundoPeso);
+        double somaPesos = primeiroPeso + segundoPeso;
+
+        double resultado = somaNotas / somaPesos;
+
+        Console.WriteLine($"A sua média ponderada é {resultado:F2}");
+
+        Console.ReadLine();
+    }
+}
