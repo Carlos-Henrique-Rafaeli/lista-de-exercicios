d6c9dc4790b9e9ea5354772a324b25c2a2518244
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 15:06:21 -0300

    Implementado exercício 05

--- Exercicio05/Program.cs
+++ Exercicio05/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Exercicio05
+﻿namespace Exercicio05;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.Write("Informe o salário base: ");
+        double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o valor total de vendas: ");
+        double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o valor da porcentagem de comissão de vendas: ");
+        double porcentagemVendas = Convert.ToDouble(Console.ReadLine());
+
+        porcentagemVendas = porcentagemVendas / 100;
+
+        double resultado = salarioBase + (totalVendas * porcentagemVendas);
+
+        Console.WriteLine($"O salário no final do mês vai ser {resultado:F2}");
+
+        Console.ReadLine();
     }
 }
