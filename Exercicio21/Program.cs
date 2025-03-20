91f8c2e528b7897717b8070e0b6a7d8923833cc8
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 17:14:59 -0300

    Implementado exercício 21

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio21;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe um valor para a calcular o fatorial: ");
+        int valorA = Convert.ToInt32(Console.ReadLine());
+
+        int valorFatorial = 1;
+
+        for (int contador = valorA; contador > 1; contador--)
+        {
+            valorFatorial *= contador;
+        }
+
+        Console.WriteLine($"O fatorial de {valorA} é {valorFatorial}");
+
+        Console.ReadLine();
+    }
+}
