aa13855e599d1e3b56ca46b9ea3d00ce3cb00145
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 16:28:30 -0300

    Implementado exercício 14

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio14;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe valor de A: ");
+        double valorA = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe valor de B: ");
+        double valorB = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe valor de C: ");
+        double valorC = Convert.ToDouble(Console.ReadLine());
+
+        double[] valoresJuntos = [valorA, valorB, valorC];
+
+        Array.Sort(valoresJuntos);
+        Array.Reverse(valoresJuntos);
+
+        Console.Write("Valores em ordem decrescente: ");
+
+        foreach(double valor in valoresJuntos)
+            Console.Write($"{valor} ");
+
+        Console.ReadLine();
+    }
+}
