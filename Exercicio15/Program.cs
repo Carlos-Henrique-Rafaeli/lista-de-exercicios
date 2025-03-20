3ef8a8c45bd01b5ef8e122a50281de08ef90919b
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 16:42:08 -0300

    Implementado exercicio 15

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercicio15;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe seu peso em Kg: ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe sua altura em metros: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double indiceMassaCorporal = peso / (Math.Pow(altura, 2));
+
+        if (indiceMassaCorporal < 18.5)
+            Console.WriteLine($"Seu IMC é de {indiceMassaCorporal:F2}, você está abaixo do peso!");
+
+        else if (indiceMassaCorporal > 18.5 && indiceMassaCorporal < 25)
+            Console.WriteLine($"Seu IMC é de {indiceMassaCorporal:F2}, você está com peso normal!");
+
+        else if (indiceMassaCorporal > 25 && indiceMassaCorporal < 30)
+            Console.WriteLine($"Seu IMC é de {indiceMassaCorporal:F2}, você está acima do peso!");
+
+        else if (indiceMassaCorporal > 30)
+            Console.WriteLine($"Seu IMC é de {indiceMassaCorporal:F2}, você está obeso!");
+
+        Console.ReadLine();
+    }
+}
