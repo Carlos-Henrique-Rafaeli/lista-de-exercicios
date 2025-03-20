03505efb77985685c4c29320b0705c1a05ded1ac
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 17:04:05 -0300

    Implementado exercício 20

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio20;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe um valor para a tabuada: ");
+        int valorA = Convert.ToInt32(Console.ReadLine());
+
+        for (int contador = 1; contador <= 10; contador++)
+        {
+            int resultado = contador * valorA;
+            Console.WriteLine($"{contador} * {valorA} = {resultado}");
+        }
+
+        Console.ReadLine();
+    }
+}
