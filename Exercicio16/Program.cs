0181caa86cd52d03deded431c152ec9022d93ca5
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-20 16:45:24 -0300

    Implementado exercício 16

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio16;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe um número para saber se ele é par ou ímpar: ");
+        int numeroInformado = Convert.ToInt32(Console.ReadLine());
+
+        if (numeroInformado % 2 == 0)
+            Console.WriteLine($"O número {numeroInformado} é par");
+        else
+            Console.WriteLine($"O número {numeroInformado} é ímpar");
+
+        Console.ReadLine();
+    }
+}
