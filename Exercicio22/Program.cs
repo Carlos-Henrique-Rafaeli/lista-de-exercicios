8ed8ace4270798103fe697940924d9b17cfa35dc
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-25 15:36:28 -0300

    Corrigido exercício 22

--- Exercicio22/Program.cs
+++ Exercicio22/Program.cs
@@ -4,20 +4,24 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Quantos número da Sequência de Fibonacci devem aparecer: ");
+        Console.Write("Digite um número: ");
         int valorA = Convert.ToInt32(Console.ReadLine());
 
         int ultimoNumero = 0;
         int penultimoNumero = 1;
 
-        for (int contador = 0; contador < valorA; contador++)
+        Console.Write($"{ultimoNumero} {penultimoNumero} ");
+
+
+        while (ultimoNumero < valorA)
         {
             int resultado = ultimoNumero + penultimoNumero;
             
+            Console.Write($"{resultado} ");
+
             penultimoNumero = ultimoNumero;
             ultimoNumero = resultado;
-            
-            Console.Write($"{ultimoNumero} ");
+
         }
 
         Console.ReadLine();
